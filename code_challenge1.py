bedd63d60fd662d6925bb8c4a02cd3d8cf17a865
Uriyann <joshuabarotea06@gmail.com>
2024-12-02 20:54:29 +0800

    fixing cc1

--- code_challenge1.py
+++ code_challenge1.py
@@ -2,4 +2,8 @@ import os
 
 os.system('cls')
 
-print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t* \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b* * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b* * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b\b\b* * * * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b* * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b* * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t*")
+print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t* \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b"
+        "* * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b* * * * * "
+        "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b\b\b* * * * * * * "
+        "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b* * * * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t\b\b"
+        "* * * \n\t\t\t\t\t\t\t\t\t\t\t\t\t*")
