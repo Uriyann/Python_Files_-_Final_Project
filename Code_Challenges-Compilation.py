77ea83def3796a403de7197090f8ba9405450dce
Uriyann <joshuabarotea06@gmail.com>
2024-12-04 21:59:58 +0800

    cc compilation commit

--- Code_Challenges-Compilation.py
+++ Code_Challenges-Compilation.py
@@ -65,3 +65,61 @@ def cc3():
         lang+" languages. You currently live in "+strt+", "+brgy+", "+city+", "+prvnc+". You currently use the phone number",
         num,"and the email "+email+". Lastly, you are a student of DLL from department of BSIT:",bsit)
 
+def cc4():
+
+    os.system('cls')
+
+    num1 = eval(input("\n\n\t\t\t\t\b\bEnter a Number: "))
+    num2 = eval(input("\t\t\t\t\b\bEnter Another Number: "))
+
+    os.system('cls')
+
+    add = num1 + num2
+    sub = num1 - num2
+    mul = num1 * num2
+    div = num1 / num2
+    mod = num1 % num2
+    exp = num1 ** num2
+    flr = num1 // num2
+
+    print("\n\n\t\t\t\t\tThe Sum of",num1,"and",num2,"is",add,"\n\n\t\t\t\t\tThe Difference of",num1,"and"
+        ,num2,"is",sub,"\n\n\t\t\t\t\tThe Product of",num1,"and",num2,"is",mul,"\n\n\t\t\t\t\tThe Quotient of"
+        ,num1,"and",num2,"is",div,"\n\n\t\t\t\t\tThe Modulus of",num1,"and",num2,"is",mod,"\n\n\t\t\t\t\tThe Exponent of"
+        ,num1,"and",num2,"is",exp,"\n\n\t\t\t\t\tThe Floor Division of",num1,"and",num2,"is",flr,"\n\n ")
+    
+def cc5():
+
+    os.system('cls')
+
+    far = float(input("\n\n\tEnter the Rate of Temperature in Farehnheit: "))
+
+    comp = (far - 32) * 5 / 9
+
+    print(f"\n\tThe Degrees of Farehnheit {far} °F converted into Celcius is {round(comp, 2)} °C")
+
+def cc6():
+
+    os.system('cls')
+
+    name = input("\n\n\tEnter Your Name: ")
+
+    pre = int(input("\n\n\tEnter Your Prelim Score: "))
+    mid = int(input("\tEnter Your Midterm Score: "))
+    senal = int(input("\tEnter Your Semi-Final Score: "))
+    fin = int(input("\tEnter Your Final Score: "))
+    quiz = int(input("\tEnter Your Quiz Score: "))
+    proj = int(input("\tEnter Your Project Grade: "))
+
+
+    fin_grade = (pre * 0.15) + (mid * 0.15) + (senal * 0.15) + (fin * 0.15) + (quiz * 0.25) + (proj * 0.15)
+
+    os.system('cls')
+
+    print(f"\n\n\tHello {name}, Your Final Grade is: {fin_grade}")
+
+    if fin_grade >= 100:
+        print("\tDid You Entered Your Real Grade?")
+    elif fin_grade >= 75:
+        print("\tCongratulations, You Are Passed!")
+    else:
+        print("\tUnfortunately, You Are Failed.")
