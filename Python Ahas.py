e91de887104cc208385d2cfc4a667bb58e04c6e1
Uriyann <joshuabarotea06@gmail.com>
2024-12-06 07:38:11 +0800

    commit

--- Python Ahas.py
+++ Python Ahas.py
@@ -1,4 +1,5 @@
-from Code_Challenges_Compilation import (cc1, cc2, cc3, cc4, cc5, cc6, cc7, cc8, cc9)
+from Code_Challenges_Compilation import (cc1, cc2, cc3, cc4, cc5, cc6, cc7, cc8, 
+                                         cc9, cc10, cc11, cc12)
 
 cc1()
 cc2()
@@ -9,3 +10,6 @@ cc6()
 cc7()
 cc8()
 cc9()
+cc10()
+cc11()
+cc12()
