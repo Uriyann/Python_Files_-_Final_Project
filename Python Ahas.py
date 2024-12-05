9cbe20f76fc9aa93acd0aff5c496453dd119dd16
Uriyann <joshuabarotea06@gmail.com>
2024-12-05 23:29:52 +0800

    small changes

--- Python Ahas.py
+++ Python Ahas.py
@@ -1,5 +1,11 @@
-from Code_Challenges_Compilation import (cc1, cc2, cc3)
+from Code_Challenges_Compilation import (cc1, cc2, cc3, cc4, cc5, cc6, cc7, cc8, cc9)
 
 cc1()
 cc2()
 cc3()
+cc4()
+cc5()
+cc6()
+cc7()
+cc8()
+cc9()
