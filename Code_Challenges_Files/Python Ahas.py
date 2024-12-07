c13ab9e8066589dafc95356e5fd30eaea8f13333
Uriyann <joshuabarotea06@gmail.com>
2024-12-07 20:40:19 +0800

    new changes

--- Code_Challenges_Files/Python Ahas.py
+++ Code_Challenges_Files/Python Ahas.py
@@ -1,5 +1,5 @@
 from Code_Challenges_Compilation import (cc1, cc2, cc3, cc4, cc5, cc6, cc7, cc8, 
-                                         cc9, cc10, cc11, cc12)
+                                         cc9, cc10, cc11, cc12, cc13, cc14, cc15, cc16)
 
 cc1()
 cc2()
@@ -13,5 +13,14 @@ cc9()
 cc10()
 cc11()
 cc12()
+cc13()
+cc14()
+cc15()
+cc16()
+
+
+
+
+
 
 #this will test all import if it works
