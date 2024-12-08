4dee0a50d7d169cecd6040af37657c0ecb56d8fb
Uriyann <joshuabarotea06@gmail.com>
2024-12-08 11:29:26 +0800

    final project draft

---
+++ Final_Project.py
@@ -0,0 +1,24 @@
+import os
+
+from Activity_Files import (
+                    Activity1, Activity2, Activity3, Activity4, Activity5,
+                    Activity6, Activity7, Activity8, Activity9, Activity10,
+                    Activity11, Activity12, Activity13, Activity14, Activity15,
+                    Activity16, Activity17, Activity18, Activity19, Activity20,
+                    Activity21, Activity22, Activity23, Activity24, Activity25
+                    )
+
+from Code_Challenges_Files import (
+                    code_challenge1, code_challenge2, activity3_BAROTEA, code_challenge4,
+                    code_challenge5, code_challenge6, code_challenge7, code_challenge8,
+                    code_challenge9, code_challenge10, code_challenge11, code_challenge12,
+                    code_challenge13, code_challenge14, code_challenge15, code_challenge16 
+                    )
+
+def clear():
+
+    os.system("cls")
+
+def Main_Menu():
+
+    print("\n")
