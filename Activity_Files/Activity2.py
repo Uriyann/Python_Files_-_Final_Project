968d92432230d4c94632a1a0c2fcca94fc7a6557
Uriyann <joshuabarotea06@gmail.com>
2024-12-15 10:56:08 +0800

    act2

--- Activity_Files/Activity2.py
+++ Activity_Files/Activity2.py
@@ -1,4 +1,5 @@
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter another number: "))
+def Act2():
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter another number: "))
 
-print(num1, "Floor Divided to", num2 ,"=", num1 // num2)
+    print(num1, "Floor Divided to", num2 ,"=", num1 // num2)
