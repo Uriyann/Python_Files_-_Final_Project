9018de5783889b23acef3f8ec7f0032a0c0335b7
Uriyann <joshuabarotea06@gmail.com>
2024-12-15 21:24:54 +0800

    act

--- Activity_Files/Activity14.py
+++ Activity_Files/Activity14.py
@@ -1,9 +1,6 @@
-for i in range(0,11):
-      print(i, end=" ")
-      for k in range(0,11):
-            print("*", end= (" "))
-      print()
-      
-
-      
-      
+def Act14():
+      for i in range(0,11):
+            print(i, end=" ")
+            for k in range(0,11):
+                  print("*", end= (" "))
+            print()
