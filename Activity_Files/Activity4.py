36d34ed95d3b0ae6aafe120f84a8b9701bfd7d05
Uriyann <joshuabarotea06@gmail.com>
2024-12-15 10:56:40 +0800

    act4

--- Activity_Files/Activity4.py
+++ Activity_Files/Activity4.py
@@ -1,23 +1,24 @@
-print("\n\n\t\b\b|======================|\n\t\b\b|  By: Joshua Barotea  |\n\t\b\b|======================|")
+def Act4():
+      print("\n\n\t\b\b|======================|\n\t\b\b|  By: Joshua Barotea  |\n\t\b\b|======================|")
 
 
 
-number1 = eval(input("\n\n\t\t\bEnter A Number: "))
-number2 = eval(input("\t\t\bEnter Another Number: "))
+      number1 = eval(input("\n\n\t\t\bEnter A Number: "))
+      number2 = eval(input("\t\t\bEnter Another Number: "))
 
-add = number1 + number2
-sub = number1 - number2
-mul = number1 * number2
-div = number1 / number2
-flr_div = number1 // number2
-exp = number1 ** number2
-mod = number1 % number2
+      add = number1 + number2
+      sub = number1 - number2
+      mul = number1 * number2
+      div = number1 / number2
+      flr_div = number1 // number2
+      exp = number1 ** number2
+      mod = number1 % number2
 
 
-print("\n\n\t\t\t\tThe sum of",number1,"and",number2,"in addition is",add,"\n\t\t\t\tThe sum of",number1,"and",number2,
-      "in subtraction is",sub,"\n\t\t\t\tThe sum of",number1,"and",number2,"in multiplication is",mul,"\n\t\t\t\tThe sum of",number1,"and",number2,
-      "in division is",div,"\n\t\t\t\tThe sum of",number1,"and",number2,"in floor division is",flr_div,"\n\t\t\t\tThe sum of",number1,"and",number2,"in exponentiation is",exp,
-      "\n\t\t\t\tThe sum of",number1,"and",number2,"in modulus is",mod,"\n\n ")
+      print("\n\n\t\t\t\tThe sum of",number1,"and",number2,"in addition is",add,"\n\t\t\t\tThe sum of",number1,"and",number2,
+            "in subtraction is",sub,"\n\t\t\t\tThe sum of",number1,"and",number2,"in multiplication is",mul,"\n\t\t\t\tThe sum of",number1,"and",number2,
+            "in division is",div,"\n\t\t\t\tThe sum of",number1,"and",number2,"in floor division is",flr_div,"\n\t\t\t\tThe sum of",number1,"and",number2,"in exponentiation is",exp,
+            "\n\t\t\t\tThe sum of",number1,"and",number2,"in modulus is",mod,"\n\n ")
 
 
 
