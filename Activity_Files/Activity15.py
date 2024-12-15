e44fafc407291aaf4d108bc6adbd7e99c1a472ec
Uriyann <joshuabarotea06@gmail.com>
2024-12-15 21:25:01 +0800

    act

--- Activity_Files/Activity15.py
+++ Activity_Files/Activity15.py
@@ -1,6 +1,7 @@
-for i in range(0, 11):
-      print(i, end=" ")
-      for k in range(0, i):
-            print("*", end= (" "))
-      print()
-      
+def Act15():
+      for i in range(0, 11):
+            print(i, end=" ")
+            for k in range(0, i):
+                  print("*", end= (" "))
+            print()
+            
--- Activity_Files/Activity16.py
+++ Activity_Files/Activity16.py
@@ -1,7 +1,8 @@
-for x in range(1, 11):
-      for z in range(1, x + 1):
-            print(" ", end=" ")
-      for y in range(11, x, -1):
-            print("*", end= " ")
+def Act16():
+      for x in range(1, 11):
+            for z in range(1, x + 1):
+                  print(" ", end=" ")
+            for y in range(11, x, -1):
+                  print("*", end= " ")
 
-      print()
+            print()
