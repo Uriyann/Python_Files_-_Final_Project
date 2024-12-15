77e20a5ab89d26038e0dd25afd5decdd4bf02af6
Uriyann <joshuabarotea06@gmail.com>
2024-12-15 10:57:06 +0800

    act6

--- Activity_Files/Activity6.py
+++ Activity_Files/Activity6.py
@@ -1,11 +1,12 @@
-x = 5
-print(x)
+def Act6():
+    x = 5
+    print(x)
 
-x += 10
-print(x)
+    x += 10
+    print(x)
 
-x += 15
-print(x)
+    x += 15
+    print(x)
 
-x+= 10
-print(x)
+    x+= 10
+    print(x)
