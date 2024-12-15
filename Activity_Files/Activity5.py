3a05478721dfc3b2a571cdad4c25d179f90266d1
Uriyann <joshuabarotea06@gmail.com>
2024-12-15 10:57:01 +0800

    act5

--- Activity_Files/Activity5.py
+++ Activity_Files/Activity5.py
@@ -1,5 +1,6 @@
-temp = float(input("Enter Temperature in Farenheit: "))
+def Act5():
+    temp = float(input("Enter Temperature in Farenheit: "))
 
-cel = (temp - 32) * 5/9
+    cel = (temp - 32) * 5/9
 
-print(f"The conversion of {temp} degrees Farenheit is {round(cel, 2)} degrees Celcius.")
+    print(f"The conversion of {temp} degrees Farenheit is {round(cel, 2)} degrees Celcius.")
