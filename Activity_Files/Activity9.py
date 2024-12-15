9c60f7830e47f8a241e61e0af9949749367d26d3
Uriyann <joshuabarotea06@gmail.com>
2024-12-15 10:57:24 +0800

    act 9

--- Activity_Files/Activity9.py
+++ Activity_Files/Activity9.py
@@ -1,20 +1,21 @@
-age = int(input("Enter your Age: "))
+def Act9():
+      age = int(input("Enter your Age: "))
 
-if age > 130:
-      print("Enter your Real Age")
-elif age >= 60 and age <= 130:
-      print("You are a senior")
-elif age >= 46 and age <= 59:
-      print("You are at Post Adulthood")
-elif age >= 32 and age <= 45:
-      print("You are at Mid Adulthood")
-elif age >= 19 and age <= 31:
-      print("You are at an Early Adulthood")
-elif age >= 14 and age <= 18:
-      print("You are a Teenager")
-elif age >= 8 and age <= 13:
-      print("You are at Pre-Teen")
-elif age >= 1 and age <= 7:
-      print("You are a Todler")
-else:
-      print("Enter a your Real Age")
+      if age > 130:
+            print("Enter your Real Age")
+      elif age >= 60 and age <= 130:
+            print("You are a senior")
+      elif age >= 46 and age <= 59:
+            print("You are at Post Adulthood")
+      elif age >= 32 and age <= 45:
+            print("You are at Mid Adulthood")
+      elif age >= 19 and age <= 31:
+            print("You are at an Early Adulthood")
+      elif age >= 14 and age <= 18:
+            print("You are a Teenager")
+      elif age >= 8 and age <= 13:
+            print("You are at Pre-Teen")
+      elif age >= 1 and age <= 7:
+            print("You are a Todler")
+      else:
+            print("Enter a your Real Age")
