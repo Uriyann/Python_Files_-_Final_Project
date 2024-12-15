f50973d59269f1717bd10b42c1e4108b6643b33a
Uriyann <joshuabarotea06@gmail.com>
2024-12-15 21:25:11 +0800

    act

--- Activity_Files/Activity18.py
+++ Activity_Files/Activity18.py
@@ -1,11 +1,12 @@
-num = int(input("Enter a number of right triangle: ")) 
+def Act18():
+      num = int(input("Enter a number of right triangle: ")) 
 
-for i in range(1, 6):
-      for r in range(1, num + 1):
-            print(" ", end= " ")
-            for j in range(1, i + 1):
-                  print("*", end= " ")
-            for k in range(5, i, -1):
-                  print(" ", end= " ") 
+      for i in range(1, 6):
+            for r in range(1, num + 1):
+                  print(" ", end= " ")
+                  for j in range(1, i + 1):
+                        print("*", end= " ")
+                  for k in range(5, i, -1):
+                        print(" ", end= " ") 
 
-      print()
+            print()
