77263518d0d1806610561f745bf859cc4689086e
Uriyann <joshuabarotea06@gmail.com>
2024-12-15 21:25:20 +0800

    act

--- Activity_Files/Activity19.py
+++ Activity_Files/Activity19.py
@@ -1,15 +1,16 @@
-contin = True
-count = 0
+def Act19():
+      contin = True
+      count = 0
 
-while contin == True:
-      name = input("Enter a name: ").upper().strip()
+      while contin == True:
+            name = input("Enter a name: ").upper().strip()
 
-      if name == "STOP":
-            print("The loop has been terminated")
-            print(f"You have entered {count} number of names")
-            break
+            if name == "STOP":
+                  print("The loop has been terminated")
+                  print(f"You have entered {count} number of names")
+                  break
 
-      else:
-            print(f"Hi {name}")
-            count += 1
-            continue
+            else:
+                  print(f"Hi {name}")
+                  count += 1
+                  continue
