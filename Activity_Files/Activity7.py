d25d8ce09c4ed9c8a078b8123fbecdfa067d560a
Uriyann <joshuabarotea06@gmail.com>
2024-12-15 10:57:11 +0800

    act7

--- Activity_Files/Activity7.py
+++ Activity_Files/Activity7.py
@@ -1,14 +1,15 @@
-gold = 0
-min = input("Hi, What is your name: ")
+def Act7():
+      gold = 0
+      min = input("Hi, What is your name: ")
 
-isgold = input("Is the mineral Gold? [Yes / No]: ").upper().strip()
+      isgold = input("Is the mineral Gold? [Yes / No]: ").upper().strip()
 
-if isgold == "YES":
-      gold += 1
-      print(f"Hi {min}, Your total number of Gold is: {gold}")
+      if isgold == "YES":
+            gold += 1
+            print(f"Hi {min}, Your total number of Gold is: {gold}")
 
-elif isgold == "NO":
-      print("Please input a Gold")
+      elif isgold == "NO":
+            print("Please input a Gold")
 
-else:
-      print("Wrong Input")
+      else:
+            print("Wrong Input")
