13c50bf9a6c166ee083c7dfb1e34a13512525fbf
Uriyann <joshuabarotea06@gmail.com>
2024-12-17 20:00:17 +0800

    modified

--- Tests_Files/mmw.py
+++ Tests_Files/mmw.py
@@ -1,14 +1,16 @@
 import os
 
 while True:
-    
-    var= int(input("Enter x: --> "))
-    os.system("cls")
+    try:
+        var= int(input("Enter x: --> "))
+        os.system("cls")
 
-    compute = ((7 * var) + 4) % 26
+        compute = ((7 * var) + 4) % 26
 
 
-    print(compute)
-    print()
+        print(compute)
+    except ValueError:
+        print("Enter a number")
+        continue
 
 """This Program Will Determine The Number For Cryptograph"""
