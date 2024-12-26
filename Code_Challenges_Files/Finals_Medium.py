7554d8bcf5d82f2071d532cf3f0f8269929a9b95
Uriyann <joshuabarotea06@gmail.com>
2024-12-26 11:10:53 +0800

    minor changes

--- Code_Challenges_Files/Finals_Medium.py
+++ Code_Challenges_Files/Finals_Medium.py
@@ -23,7 +23,7 @@ toppings = {
 price = 0
 
 while True:
-    pizza = input("Would you like to purchase/buy a pizza: ").strip().upper()
+    pizza = input("\nWould you like to purchase/buy a pizza: ").strip().upper()
     
     if pizza == "NO":
         break
@@ -36,24 +36,24 @@ while True:
         calc = flavors[flavor_pizz] + sizes[sizes_pizz] + toppings[topps_pizz]
         price += calc
 
-        print("Order Summary: "
+        print("\n\nOrder Summary: "
               f"\nFlavor: {flavor_pizz}"
               f"\nSize: {sizes_pizz}"
               f"\nToppings: {topps_pizz}"
               
               f"\n\nPrice: {price}")
         
-        pizza1 = input("Would you like to purchase/buy another pizza: ").strip().upper()
+        pizza1 = input("\nWould you like to purchase/buy another pizza: ").strip().upper()
 
         if pizza1 == "YES":
-            pizza
+            continue
 
         elif pizza1 == "NO":
             break
         
 
     else:
-        print("We don't have pizza that you like")
+        print("\nWe don't have pizza that you like")
         continue
 
         
