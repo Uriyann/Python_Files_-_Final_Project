3710427dc845c92932249ea7c0ccbda42465287f
Uriyann <joshuabarotea06@gmail.com>
2024-12-26 19:54:44 +0800

    finals medium

--- Code_Challenges_Files/Finals_Medium.py
+++ Code_Challenges_Files/Finals_Medium.py
@@ -1,35 +1,36 @@
 flavors = {
-    "Pepperoni": 200,
-    "Hawaiian": 250,
-    "Supreme": 300,
-    "Cheese": 180
+    "Pepperoni": 12,
+    "Hawaiian": 10,
+    "Bolognese": 5,
+    "Overload": 20
 }
 
 sizes = {
-    "Small": 50,
-    "Medium": 100,
-    "Large": 150,
-    "Extra Large": 200
+    "Small": 5,
+    "Medium": 7,
+    "Large": 10,
+    "Extra Large": 15
 }
 
 toppings = {
-    "Olives": 30,
-    "Mushrooms": 40,
-    "Extra Cheese": 50,
-    "Bacon": 60,
+    "Olives": 2,
+    "Mushrooms": 2,
+    "Extra Cheese": 2,
+    "Bacon": 2,
     "None": 0
 }
 
 price = 0
 
 while True:
-    pizza = input("\nWould you like to purchase/buy a pizza: ").strip().upper()
+    pizza = input("\nWould you like to purchase/buy a pizza [Yes/No]: ").strip().upper()
     
     if pizza == "NO":
+        print("\nThank you for using the machine")
         break
 
     elif pizza == "YES":
-        flavor_pizz = input(f"Choose: {flavors}: ").strip()
+        flavor_pizz = input(f"\nChoose: {flavors}: ").strip()
         sizes_pizz = input(f"Choose: {sizes}: ").strip()
         topps_pizz = input(f"Choose: {toppings}: ").strip()
 
@@ -41,16 +42,21 @@ while True:
               f"\nSize: {sizes_pizz}"
               f"\nToppings: {topps_pizz}"
               
-              f"\n\nPrice: {price}")
+              f"\n\nPrice: ${price}")
         
-        pizza1 = input("\nWould you like to purchase/buy another pizza: ").strip().upper()
+        pizza1 = input("\nWould you like to purchase/buy another pizza [Yes/No]: ").strip().upper()
 
         if pizza1 == "YES":
             continue
 
         elif pizza1 == "NO":
+            print("\n\nThank you for ordering")
+            print(f"\nThe total price of the order is: ${price}")
             break
         
+        else:
+            print("\nWe don't have pizza that you like")
+            continue
 
     else:
         print("\nWe don't have pizza that you like")
