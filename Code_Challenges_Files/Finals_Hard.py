545520b76b9c7cade7df109b23c607e0c14d5c93
Uriyann <joshuabarotea06@gmail.com>
2024-12-27 13:03:59 +0800

    finished hard finals

--- Code_Challenges_Files/Finals_Hard.py
+++ Code_Challenges_Files/Finals_Hard.py
@@ -1,4 +1,4 @@
-amount = int(input("Enter the loan amount: "))
+amount = int(input("\nEnter the loan amount: "))
 period = int(input("Enter the load period in years: "))
 
 annual = 0.03
@@ -10,8 +10,8 @@ remaining_loan = amount
 
 print("\n\n")
 
-for headers in range(1, 2):
-    print("\t\b\bMonth", end= "\t|")
+for headers in range(1):
+    print("|\t\b\bMonth", end= "\t|")
 
     for monthly_pay_headers in range(1):
         print("\tMonthly Payment\t", end= "\t|")
@@ -27,31 +27,35 @@ for headers in range(1, 2):
 
     print()
 
-for month in range(1, months + 1):
-    print(f"\t{month}", end= "\t|")
+for lines in range(129):
+    print("-", end="")
 
+print()
+
+for month in range(1, months + 1):
+    print(f"|\t{month}", end= "\t|")
     interest_payment = remaining_loan * interest
 
     monthly_payment = principal + interest_payment
 
     remaining_loan -= principal
 
-    monthly_payment = round(monthly_payment, 2)
-    interest_payment = round(interest_payment, 2)
-    principal = round(principal, 2)
+    monthly_payment_str = f"{monthly_payment:.2f}"
+    interest_payment_str = f"{interest_payment:.2f}"
+    principal_str = f"{principal:.2f}"
     remaining_loan = abs(remaining_loan)
-    remaining_loan = round(remaining_loan, 2)
+    remaining_loan_str = f"{remaining_loan:.2f}"
 
     for monthly_pay in range(1):
-        print(f"\t    {monthly_payment}", end="\t\t|")
+        print(f"\t    {monthly_payment_str}", end="\t\t|")
     
     for interest_pay in range(1):
-        print(f"\t  {interest_payment}\t", end="\t|")
+        print(f"\t  {interest_payment_str}\t", end="\t|")
 
     for princ in range(1):
-        print(f"\t  {principal}\t", end="\t|")
+        print(f"\t  {principal_str}\t", end="\t|")
 
     for remaining in range(1):
-        print(f"\t    {remaining_loan}\t", end="\t|")
+        print(f"\t    {remaining_loan_str} \t", end="\t|")
 
     print()
